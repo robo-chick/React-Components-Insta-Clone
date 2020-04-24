700015d00b8c6d91443c40fff531da4ab9398002
Tasha <tashingsworth@gmail.com>
2020-04-24 17:05:55 -0400

    I think I've finished MVP, lookign it over and then will look at stretch

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -2,24 +2,21 @@
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+
 
 const LikeSection = props => {
   return (
     <div>
-      <div
-        className="like-section"
-        key="likes-icons-container"
-      >
+      <div className="like-section" key="likes-icons-container" onClick={props.increaseLikes}>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <i className='far fa-heart' />
         </div>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
+          <i className='far fa-comment' />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+     
+      <p className="like-number">{props.likes} likes</p>
     </div>
   )
 };
